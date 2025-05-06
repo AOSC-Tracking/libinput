sizeofvoid/libopeninput

commit 5324f425a1635fb95356461c34e43d72590c9023

Introduce custom acceleration profile
The custom acceleration profile allow the user to define custom
acceleration functions for each movement type per device, giving
full control over accelerations behavior at different speeds.

This commit introduces 2 movement types which corresponds to the
2 profiles currently in use by libinput.

regular filter is Motion type.
constant filter is Fallback type.

This allows possible expansion of new movement types for the
different devices.

The custom pointer acceleration profile gives the user full control over the
acceleration behavior at different speeds.
The user needs to provide a custom acceleration function f(x) where
the x-axis is the device speed and the y-axis is the pointer speed.

The user should take into account the native device dpi and screen dpi in
order to achieve the desired behavior/feel of the acceleration.

The custom acceleration function is defined using n points which are spaced
uniformly along the x-axis, starting from 0 and continuing in constant steps.
There by the points defining the custom function are:
(0 * step, f[0]), (1 * step, f[1]), ..., ((n-1) * step, f[n-1])
where f is a list of n unitless values defining the acceleration
factor for each velocity.
When a velocity value does not lie exactly on those points, a linear
interpolation of the two closest points will be calculated.
When a velocity value is greater than the max point defined, a linear
extrapolation of the two biggest points will be calculated.

Signed-off-by: Yinon Burgansky <51504-Yinon@users.noreply.gitlab.freedesktop.org>
Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

src/libinput.c
@@ -4159,6 +4159,7 @@ libinput_device_config_accel_set_profile(struct libinput_device *device,
 	switch (profile) {
 	case LIBINPUT_CONFIG_ACCEL_PROFILE_FLAT:
 	case LIBINPUT_CONFIG_ACCEL_PROFILE_ADAPTIVE:
+	case LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM:
 		break;
 	default:
 		return LIBINPUT_CONFIG_STATUS_INVALID;
@@ -4169,6 +4170,121 @@ libinput_device_config_accel_set_profile(struct libinput_device *device,
 		return LIBINPUT_CONFIG_STATUS_UNSUPPORTED;
 
 	return device->config.accel->set_profile(device, profile);
+}
+
+static inline struct libinput_config_accel_custom_func *
+libinput_config_accel_custom_func_create(void)
+{
+	struct libinput_config_accel_custom_func *func = zalloc(sizeof(*func));
+
+	func->step = 1.0;
+	func->npoints = 2;
+	func->points[0] = 0.0; /* default to a flat unaccelerated function */
+	func->points[1] = 1.0;
+
+	return func;
+}
+
+static inline void
+libinput_config_accel_custom_func_destroy(struct libinput_config_accel_custom_func * func)
+{
+	free(func);
+}
+
+LIBINPUT_EXPORT struct libinput_config_accel *
+libinput_config_accel_create(enum libinput_config_accel_profile profile)
+{
+	struct libinput_config_accel *config = zalloc(sizeof(*config));
+
+	config->profile = profile;
+
+	switch (profile) {
+	case LIBINPUT_CONFIG_ACCEL_PROFILE_NONE:
+		break;
+	case LIBINPUT_CONFIG_ACCEL_PROFILE_FLAT:
+	case LIBINPUT_CONFIG_ACCEL_PROFILE_ADAPTIVE:
+		return config;
+	case LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM:
+		config->custom.fallback = libinput_config_accel_custom_func_create();
+		return config;
+	}
+
+	free(config);
+	return NULL;
+}
+
+LIBINPUT_EXPORT void
+libinput_config_accel_destroy(struct libinput_config_accel *accel_config)
+{
+	libinput_config_accel_custom_func_destroy(accel_config->custom.fallback);
+	libinput_config_accel_custom_func_destroy(accel_config->custom.motion);
+	free(accel_config);
+}
+
+LIBINPUT_EXPORT enum libinput_config_status
+libinput_device_config_accel_apply(struct libinput_device *device,
+				   struct libinput_config_accel *accel_config)
+{
+	enum libinput_config_status status;
+	status = libinput_device_config_accel_set_profile(device, accel_config->profile);
+	if (status != LIBINPUT_CONFIG_STATUS_SUCCESS)
+		return status;
+
+	switch (accel_config->profile) {
+	case LIBINPUT_CONFIG_ACCEL_PROFILE_FLAT:
+	case LIBINPUT_CONFIG_ACCEL_PROFILE_ADAPTIVE:
+	{
+		double speed = libinput_device_config_accel_get_default_speed(device);
+		return libinput_device_config_accel_set_speed(device, speed);
+	}
+	case LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM:
+		return device->config.accel->set_accel_config(device, accel_config);
+
+	default:
+		return LIBINPUT_CONFIG_STATUS_INVALID;
+	}
+}
+
+LIBINPUT_EXPORT enum libinput_config_status
+libinput_config_accel_set_points(struct libinput_config_accel *config,
+				 enum libinput_config_accel_type accel_type,
+				 double step, size_t npoints, double *points)
+{
+	if (config->profile != LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM)
+		return LIBINPUT_CONFIG_STATUS_INVALID;
+
+	switch (accel_type) {
+	case LIBINPUT_ACCEL_TYPE_FALLBACK:
+	case LIBINPUT_ACCEL_TYPE_MOTION:
+		break;
+	default:
+		return LIBINPUT_CONFIG_STATUS_INVALID;
+	}
+
+	if (step <= 0 || step > LIBINPUT_ACCEL_STEP_MAX)
+		return LIBINPUT_CONFIG_STATUS_INVALID;
+
+	if (npoints < LIBINPUT_ACCEL_NPOINTS_MIN || npoints > LIBINPUT_ACCEL_NPOINTS_MAX)
+		return LIBINPUT_CONFIG_STATUS_INVALID;
+
+	struct libinput_config_accel_custom_func *func = libinput_config_accel_custom_func_create();
+
+	func->step = step;
+	func->npoints = npoints;
+	memcpy(func->points, points, sizeof(*points) * npoints);
+
+	switch (accel_type) {
+	case LIBINPUT_ACCEL_TYPE_FALLBACK:
+		libinput_config_accel_custom_func_destroy(config->custom.fallback);
+		config->custom.fallback = func;
+		break;
+	case LIBINPUT_ACCEL_TYPE_MOTION:
+		libinput_config_accel_custom_func_destroy(config->custom.motion);
+		config->custom.motion = func;
+		break;
+	}
+
+	return LIBINPUT_CONFIG_STATUS_SUCCESS;
 }
 
 LIBINPUT_EXPORT int

src/libinput.h
@@ -5172,6 +5172,12 @@ libinput_device_config_accel_set_speed(struct libinput_device *device,
  * returned value is normalized to a range of [-1, 1].
  * See libinput_device_config_accel_set_speed() for details.
  *
+ * If the current acceleration profile is @ref
+ * LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM, the behavior of the
+ * device will not change but future calls to
+ * libinput_device_config_accel_get_speed() will reflect the updated speed
+ * setting.
+ *
  * @param device The device to configure
  *
  * @return The current speed, range -1 to 1
@@ -5225,7 +5231,175 @@ enum libinput_config_accel_profile {
 	 * on the input speed. This is the default profile for most devices.
 	 */
 	LIBINPUT_CONFIG_ACCEL_PROFILE_ADAPTIVE = (1 << 1),
+
+	/**
+	 * A custom acceleration profile. Device movement acceleration depends
+	 * on user defined custom acceleration functions for each movement
+	 * type.
+	 *
+	 * @see libinput_device_config_accel_set_points
+	 */
+	LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM = (1 << 2),
 };
+
+/**
+ * @ingroup config
+ *
+ * A handle for configuration pointer acceleration.
+ *
+ * @warning Unlike other structs pointer acceleration configuration is
+ * considered transient and <b>not</b> refcounted. Calling
+ * libinput_config_accel_destroy() <b>will</b> destroy the configuration.
+ *
+ * To configure pointer acceleration, first create a config of a desired
+ * acceleration profile with libinput_config_accel_create(), then
+ * configure the profile-specific acceleration properties.
+ *
+ * In this version of libinput, this pointer acceleration configuration
+ * only provides configuration for @ref LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM.
+ *
+ * For @ref LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM use
+ * @ref libinput_config_accel_set_points.
+ *
+ * Once set up, apply the configuration to a device using
+ * libinput_device_config_accel_apply(). Once applied,
+ * destroy it with libinput_config_accel_destroy().
+ *
+ * @since 1.23
+ */
+struct libinput_config_accel;
+
+/**
+ * @ingroup config
+ *
+ * Create an acceleration configuration of a given profile.
+ *
+ * Note that in this version of libinput, only the
+ * @ref LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM profile provides configuration
+ * options. All other acceleration profiles, when applied, will merely switch
+ * the profile and reset any profile-specific options to the default values.
+ *
+ * @param profile The profile of the newly created acceleration configuration.
+ *
+ * @return The newly created acceleration configuration or NULL on error.
+ *
+ * @warning Unlike other structs pointer acceleration configuration is
+ * considered transient and <b>not</b> refcounted. Calling
+ * libinput_config_accel_destroy() <b>will</b> destroy the configuration.
+ *
+ * @see libinput_config_accel
+ * @since 1.23
+ */
+struct libinput_config_accel *
+libinput_config_accel_create(enum libinput_config_accel_profile profile);
+
+/**
+ * @ingroup config
+ *
+ * Destroy an acceleration configuration.
+ *
+ * @warning Unlike other structs pointer acceleration configuration is
+ * considered transient and <b>not</b> refcounted. Calling
+ * libinput_config_accel_destroy() <b>will</b> destroy the configuration.
+ *
+ * @param accel_config The acceleration configuration to destroy.
+ *
+ * @see libinput_config_accel
+ * @since 1.23
+ */
+void
+libinput_config_accel_destroy(struct libinput_config_accel *accel_config);
+
+/**
+ * @ingroup config
+ *
+ * Apply this pointer acceleration configuration to the device. This changes the
+ * device's pointer acceleration method to the method given in
+ * libinput_config_accel_create() and applies all other configuration settings.
+ *
+ * Once applied, call libinput_config_accel_destroy() to destroy the
+ * configuration struct.
+ *
+ * @param device The device to configure.
+ * @param accel_config The acceleration configuration.
+ *
+ * @return A config status code.
+ *
+ * @see libinput_config_accel
+ * @since 1.23
+ */
+enum libinput_config_status
+libinput_device_config_accel_apply(struct libinput_device *device,
+				   struct libinput_config_accel *accel_config);
+
+/**
+ * @ingroup config
+ *
+ * Acceleration types are categories of movement by a device that may have
+ * specific acceleration functions applied. A device always supports the
+ * @ref LIBINPUT_ACCEL_TYPE_MOTION type (for regular pointer motion). Other
+ * types (e.g. scrolling) may be added in the future.
+ *
+ * The special type @ref LIBINPUT_ACCEL_TYPE_FALLBACK specifies the acceleration
+ * function to be moved for any movement produced by the device that does not
+ * have a specific acceleration type defined.
+ *
+ * Use to specify the acceleration function type in
+ * @ref libinput_config_accel_set_points
+ *
+ * Each device implements a subset of those types, see a list of supported
+ * devices for each movement type definition.
+ *
+ * @see LIBINPUT_ACCEL_ARG_TYPE
+ * @since 1.23
+ */
+enum libinput_config_accel_type {
+	/**
+	 * The default acceleration type used as a fallback when other
+	 * acceleration types are not provided.
+	 */
+	LIBINPUT_ACCEL_TYPE_FALLBACK = 0,
+	/**
+	 * Acceleration type for regular pointer movement. This
+	 * type is always supported.
+	 */
+	LIBINPUT_ACCEL_TYPE_MOTION,
+};
+
+/**
+ * @ingroup config
+ *
+ * Defines the acceleration function for a given movement type
+ * in an acceleration configuration with the profile
+ * @ref LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM.
+ *
+ * Movement types are specific to each device, @see libinput_config_accel_type.
+ *
+ * Each custom acceleration function is defined by ``n`` points spaced uniformly
+ * along the x-axis starting from 0 and continuing in a constant step size.
+ * There by the function is defined by the following points:
+ * (0 * step, f[0]), (1 * step, f[1]), ..., ((n - 1) * step, f[n - 1]).
+ * The x-axis represents the device-speed in device units per millisecond.
+ * The y-axis represents the pointer-speed.
+ *
+ * It is up to the user to define those values in accordance with device DPI
+ * and screen DPI.
+ *
+ * @param accel_config The acceleration configuration to modify.
+ * @param accel_type The movement type to configure a custom function for.
+ * @param step The distance between each point along the x-axis.
+ * @param npoints The number of points of the custom acceleration function.
+ * @param points The points' y-values of the custom acceleration function.
+ *
+ * @return A config status code.
+ *
+ * @see libinput_config_accel
+ * @since 1.23
+ */
+enum libinput_config_status
+libinput_config_accel_set_points(struct libinput_config_accel *accel_config,
+				 enum libinput_config_accel_type accel_type,
+				 double step, size_t npoints, double *points);
 
 /**
  * @ingroup config

meson.build
@@ -292,6 +292,7 @@ dep_libinput_util = declare_dependency(link_with : libinput_util)
 ############ libfilter.a ############
 src_libfilter = [
 		'src/filter.c',
+		'src/filter-custom.c',
 		'src/filter-flat.c',
 		'src/filter-low-dpi.c',
 		'src/filter-mouse.c',

src/evdev-mt-touchpad.c
@@ -2988,18 +2988,26 @@ tp_init_accel(struct tp_dispatch *tp, enum libinput_config_accel_profile which)
 	tp->accel.y_scale_coeff = (DEFAULT_MOUSE_DPI/25.4) / res_y;
 	tp->accel.xy_scale_coeff = 1.0 * res_x/res_y;
 
-	if (which == LIBINPUT_CONFIG_ACCEL_PROFILE_FLAT)
+	if (which == LIBINPUT_CONFIG_ACCEL_PROFILE_FLAT) {
 		filter = create_pointer_accelerator_filter_touchpad_flat(dpi);
-	else if (evdev_device_has_model_quirk(device, QUIRK_MODEL_LENOVO_X230) ||
-		 tp->device->model_flags & EVDEV_MODEL_LENOVO_X220_TOUCHPAD_FW81)
+	} else if (which == LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM) {
+		filter = create_custom_accelerator_filter();
+	} else if (evdev_device_has_model_quirk(device, QUIRK_MODEL_LENOVO_X230) ||
+		 tp->device->model_flags & EVDEV_MODEL_LENOVO_X220_TOUCHPAD_FW81) {
 		filter = create_pointer_accelerator_filter_lenovo_x230(dpi, use_v_avg);
-	else if (libevdev_get_id_bustype(device->evdev) == BUS_BLUETOOTH)
+	} else {
+		uint64_t eds_threshold = 0;
+		uint64_t eds_value = 0;
+
+		if (libevdev_get_id_bustype(device->evdev) == BUS_BLUETOOTH) {
+			eds_threshold = ms2us(50);
+			eds_value = ms2us(10);
+		}
 		filter = create_pointer_accelerator_filter_touchpad(dpi,
-								    ms2us(50),
-								    ms2us(10),
+								    eds_threshold,
+								    eds_value,
 								    use_v_avg);
-	else
-		filter = create_pointer_accelerator_filter_touchpad(dpi, 0, 0, use_v_avg);
+	}
 
 	if (!filter)
 		return false;

src/evdev.c
@@ -1187,7 +1187,9 @@ evdev_init_accel(struct evdev_device *device,
 {
 	struct motion_filter *filter = NULL;
 
-	if (device->tags & EVDEV_TAG_TRACKPOINT) {
+	if (which == LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM)
+		filter = create_custom_accelerator_filter();
+	else if (device->tags & EVDEV_TAG_TRACKPOINT) {
 		if (which == LIBINPUT_CONFIG_ACCEL_PROFILE_FLAT)
 			filter = create_pointer_accelerator_filter_trackpoint_flat(device->trackpoint_multiplier);
 		else
@@ -1255,7 +1257,8 @@ evdev_accel_config_get_profiles(struct libinput_device *libinput_device)
 		return LIBINPUT_CONFIG_ACCEL_PROFILE_NONE;
 
 	return LIBINPUT_CONFIG_ACCEL_PROFILE_ADAPTIVE |
-		LIBINPUT_CONFIG_ACCEL_PROFILE_FLAT;
+	       LIBINPUT_CONFIG_ACCEL_PROFILE_FLAT |
+	       LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM;
 }
 
 static enum libinput_config_status
@@ -1304,6 +1307,20 @@ evdev_accel_config_get_default_profile(struct libinput_device *libinput_device)
 	return LIBINPUT_CONFIG_ACCEL_PROFILE_ADAPTIVE;
 }
 
+static enum libinput_config_status
+evdev_set_accel_config(struct libinput_device *libinput_device,
+		       struct libinput_config_accel *accel_config)
+{
+	assert(evdev_accel_config_get_profile(libinput_device) == accel_config->profile);
+
+	struct evdev_device *dev = evdev_device(libinput_device);
+
+	if (!filter_set_accel_config(dev->pointer.filter, accel_config))
+		return LIBINPUT_CONFIG_STATUS_INVALID;
+
+	return LIBINPUT_CONFIG_STATUS_SUCCESS;
+}
+
 void
 evdev_device_init_pointer_acceleration(struct evdev_device *device,
 				       struct motion_filter *filter)
@@ -1321,6 +1338,7 @@ evdev_device_init_pointer_acceleration(struct evdev_device *device,
 		device->pointer.config.set_profile = evdev_accel_config_set_profile;
 		device->pointer.config.get_profile = evdev_accel_config_get_profile;
 		device->pointer.config.get_default_profile = evdev_accel_config_get_default_profile;
+		device->pointer.config.set_accel_config = evdev_set_accel_config;
 		device->base.config.accel = &device->pointer.config;
 
 		default_speed = evdev_accel_config_get_default_speed(&device->base);

src/filter-custom.c
@@ -1,0 +1,393 @@
+/*
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "config.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "filter.h"
+#include "filter-private.h"
+
+#define MOTION_TIMEOUT ms2us(1000)
+#define FIRST_MOTION_TIME_INTERVAL ms2us(7) /* random but good enough interval for very first event */
+
+struct custom_accel_function {
+	uint64_t last_time;
+	double step;
+	size_t npoints;
+	double points[];
+};
+
+static struct custom_accel_function *
+create_custom_accel_function(double step, const double *points, size_t npoints)
+{
+	if (npoints < LIBINPUT_ACCEL_NPOINTS_MIN ||
+	    npoints > LIBINPUT_ACCEL_NPOINTS_MAX)
+		return NULL;
+
+	if (step <= 0 || step > LIBINPUT_ACCEL_STEP_MAX)
+		return NULL;
+
+	struct custom_accel_function *cf = zalloc(sizeof(*cf) + npoints * sizeof(*points));
+	cf->last_time = 0;
+	cf->step = step;
+	cf->npoints = npoints;
+	memcpy(cf->points, points, sizeof(*points) * npoints);
+
+	return cf;
+}
+
+static void
+custom_accel_function_destroy(struct custom_accel_function *cf)
+{
+	if (cf == NULL)
+		return;
+
+	free(cf);
+}
+
+static double
+custom_accel_function_calculate_speed(struct custom_accel_function *cf,
+				      const struct device_float_coords *unaccelerated,
+				      uint64_t time)
+{
+	/* Although most devices have a constant polling rate, and for fast
+	 * movements these distances do represent the actual speed,
+	 * for slow movements it is not the case.
+	 *
+	 * Since all devices have a finite resolution, real world events
+	 * for a slow smooth movement could look like:
+	 *   Event 1 - (0,  1) - time 0
+	 *   Event 2 - (0,  0) - time 7  - filtered (zero event)
+	 *   Event 3 - (1,  0) - time 14
+	 *   Event 4 - (0,  0) - time 21 - filtered (zero event)
+	 *   Event 5 - (0,  0) - time 28 - filtered (zero event)
+	 *   Event 6 - (0,  1) - time 35
+	 *
+	 * Not taking the time into account would mean interpreting those events as:
+	 *   Move 1 unit over 7 ms
+	 *   Pause for 7 ms
+	 *   Move 1 unit over 7 ms
+	 *   Pause for 14 ms
+	 *   Move 1 unit over 7ms
+	 *
+	 * Where in reality this was one smooth movement without pauses,
+	 * so after normalizing for time we get:
+	 *   Move 1 unit over 7 ms
+	 *   Move 1 unit over 14 ms
+	 *   Move 1 unit over 21ms
+	 *
+	 * which should give us better speed estimation.
+	 */
+
+	/* calculate speed based on time passed since last event */
+	double distance = hypot(unaccelerated->x, unaccelerated->y);
+	/* handle first event in a motion */
+	if (time - cf->last_time > MOTION_TIMEOUT)
+		cf->last_time = time - FIRST_MOTION_TIME_INTERVAL;
+
+	double dt = us2ms_f(time - cf->last_time);
+	double speed = distance / dt; /* speed is in device-units per ms */
+	cf->last_time = time;
+
+	return speed;
+}
+
+static double
+custom_accel_function_profile(struct custom_accel_function *cf,
+			      double speed_in)
+{
+	size_t npoints = cf->npoints;
+	double step = cf->step;
+	double *points = cf->points;
+
+	/* calculate the index of the first point used for interpolation */
+	size_t i = speed_in / step;
+
+	/* if speed is greater than custom curve's max speed,
+	   use last 2 points for linear extrapolation
+	   (same calculation as linear interpolation) */
+	i = min(i, npoints - 2);
+
+	/* the 2 points used for linear interpolation */
+	double x0 = step * i;
+	double x1 = step * (i + 1);
+	double y0 = points[i];
+	double y1 = points[i + 1];
+
+	/* linear interpolation */
+	double speed_out = (y0 * (x1 - speed_in) + y1 * (speed_in - x0)) / step;
+
+	/* We moved (dx, dy) device units within the last N ms. This gives us a
+	 * given speed S in units/ms, that's our accel input. Our curve says map
+	 * that speed S to some other speed S'.
+	 *
+	 * Our device delta is represented by the vector, that vector needs to
+	 * be modified to represent our intended speed.
+	 *
+	 * Example: we moved a delta of 7 over the last 7ms. Our speed is
+	 * thus 1 u/ms, our out speed is 2 u/ms because we want to double our
+	 * speed (points: [0.0, 2.0]). Our delta must thus be 14 - factor of 2,
+	 * or out-speed/in-speed.
+	 *
+	 * Example: we moved a delta of 1 over the last 7ms. Our input speed is
+	 * 1/7 u/ms, our out speed is 1/7ms because we set up a flat accel
+	 * curve (points: [0.0, 1.0]). Our delta must thus be 1 - factor of 1,
+	 * or out-speed/in-speed.
+	 *
+	 * Example: we moved a delta of 1 over the last 21ms. Our input speed is
+	 * 1/21 u/ms, our out speed is 1u/ms because we set up a fixed-speed
+	 * curve (points: [1.0, 1.0]). Our delta must thus be 21 - factor of 21,
+	 * or out-speed/in-speed.
+	 *
+	 * Example: we moved a delta of 21 over the last 7ms. Our input speed is
+	 * 3 u/ms, our out speed is 1u/ms because we set up a fixed-speed
+	 * curve (points: [1.0, 1.0]). Our delta must thus be 7 - factor of 1/3,
+	 * or out-speed/in-speed.
+	 */
+
+	/* calculate the acceleration factor based on the user desired speed out */
+	double accel_factor = speed_out / speed_in;
+
+	return accel_factor;
+}
+
+static struct normalized_coords
+custom_accel_function_filter(struct custom_accel_function *cf,
+			     const struct device_float_coords *unaccelerated,
+			     uint64_t time)
+{
+	double speed = custom_accel_function_calculate_speed(cf, unaccelerated, time);
+
+	double accel_factor = custom_accel_function_profile(cf, speed);
+
+	struct normalized_coords accelerated = {
+		.x = unaccelerated->x * accel_factor,
+		.y = unaccelerated->y * accel_factor,
+	};
+
+	return accelerated;
+}
+
+struct custom_accelerator {
+	struct motion_filter base;
+	struct {
+		struct custom_accel_function *fallback;
+		struct custom_accel_function *motion;
+	} funcs;
+};
+
+static struct custom_accel_function *
+custom_accelerator_get_custom_function(struct custom_accelerator *f,
+				       enum libinput_config_accel_type accel_type)
+{
+	switch (accel_type) {
+	case LIBINPUT_ACCEL_TYPE_FALLBACK:
+		return f->funcs.fallback;
+	case LIBINPUT_ACCEL_TYPE_MOTION:
+		return f->funcs.motion ? f->funcs.motion : f->funcs.fallback;
+	}
+
+	return f->funcs.fallback;
+}
+
+static double
+custom_accelerator_profile(enum libinput_config_accel_type accel_type,
+			   struct motion_filter *filter,
+			   double speed_in)
+{
+	struct custom_accelerator *f = (struct custom_accelerator *)filter;
+	struct custom_accel_function *cf;
+
+	cf = custom_accelerator_get_custom_function(f, accel_type);
+
+	return custom_accel_function_profile(cf, speed_in);
+}
+
+static struct normalized_coords
+custom_accelerator_filter(enum libinput_config_accel_type accel_type,
+			  struct motion_filter *filter,
+			  const struct device_float_coords *unaccelerated,
+			  uint64_t time)
+{
+	struct custom_accelerator *f = (struct custom_accelerator *)filter;
+	struct custom_accel_function *cf;
+
+	cf = custom_accelerator_get_custom_function(f, accel_type);
+
+	return custom_accel_function_filter(cf, unaccelerated, time);
+}
+
+static void
+custom_accelerator_restart(struct motion_filter *filter,
+			   void *data,
+			   uint64_t time)
+{
+	/* noop, this function has no effect in the custom interface */
+}
+
+static void
+custom_accelerator_destroy(struct motion_filter *filter)
+{
+	struct custom_accelerator *f =
+		(struct custom_accelerator *)filter;
+
+	/* destroy all custom movement functions */
+	custom_accel_function_destroy(f->funcs.fallback);
+	custom_accel_function_destroy(f->funcs.motion);
+	free(f);
+}
+
+static bool
+custom_accelerator_set_speed(struct motion_filter *filter,
+			     double speed_adjustment)
+{
+	assert(speed_adjustment >= -1.0 && speed_adjustment <= 1.0);
+
+	/* noop, this function has no effect in the custom interface */
+
+	return true;
+}
+
+static bool
+custom_accelerator_set_accel_config(struct motion_filter *filter,
+				    struct libinput_config_accel *config)
+{
+	struct custom_accelerator *f =
+		(struct custom_accelerator *)filter;
+
+	struct custom_accel_function *fallback = NULL,
+				     *motion = NULL;
+
+	if (config->custom.fallback) {
+		fallback = create_custom_accel_function(config->custom.fallback->step,
+							config->custom.fallback->points,
+							config->custom.fallback->npoints);
+		if (!fallback)
+			goto out;
+	}
+
+	if (config->custom.motion) {
+		motion = create_custom_accel_function(config->custom.motion->step,
+						      config->custom.motion->points,
+						      config->custom.motion->npoints);
+		if (!motion)
+			goto out;
+	}
+
+	custom_accel_function_destroy(f->funcs.fallback);
+	custom_accel_function_destroy(f->funcs.motion);
+
+	f->funcs.fallback = fallback;
+	f->funcs.motion = motion;
+
+	return true;
+
+out:
+	custom_accel_function_destroy(fallback);
+	custom_accel_function_destroy(motion);
+
+	return false;
+}
+
+/* custom profiles and filters for the different accel types: */
+
+double
+custom_accel_profile_fallback(struct motion_filter *filter,
+			      void *data,
+			      double speed_in,
+			      uint64_t time)
+{
+	return custom_accelerator_profile(LIBINPUT_ACCEL_TYPE_FALLBACK,
+					  filter,
+					  speed_in);
+}
+
+static struct normalized_coords
+custom_accelerator_filter_fallback(struct motion_filter *filter,
+				   const struct device_float_coords *unaccelerated,
+				   void *data,
+				   uint64_t time)
+{
+	return custom_accelerator_filter(LIBINPUT_ACCEL_TYPE_FALLBACK,
+					 filter,
+					 unaccelerated,
+					 time);
+}
+
+double
+custom_accel_profile_motion(struct motion_filter *filter,
+			    void *data,
+			    double speed_in,
+			    uint64_t time)
+{
+	return custom_accelerator_profile(LIBINPUT_ACCEL_TYPE_MOTION,
+					  filter,
+					  speed_in);
+}
+
+static struct normalized_coords
+custom_accelerator_filter_motion(struct motion_filter *filter,
+				 const struct device_float_coords *unaccelerated,
+				 void *data,
+				 uint64_t time)
+{
+	return custom_accelerator_filter(LIBINPUT_ACCEL_TYPE_MOTION,
+					 filter,
+					 unaccelerated,
+					 time);
+}
+
+struct motion_filter_interface custom_accelerator_interface = {
+	.type = LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM,
+	.filter = custom_accelerator_filter_motion,
+	.filter_constant = custom_accelerator_filter_fallback,
+	.restart = custom_accelerator_restart,
+	.destroy = custom_accelerator_destroy,
+	.set_speed = custom_accelerator_set_speed,
+	.set_accel_config = custom_accelerator_set_accel_config,
+};
+
+struct motion_filter *
+create_custom_accelerator_filter(void)
+{
+	struct custom_accelerator *f = zalloc(sizeof(*f));
+
+	/* the unit function by default, speed in = speed out,
+	   i.e. no acceleration */
+	const double default_step = 1.0;
+	const double default_points[2] = {0.0, 1.0};
+
+	/* initialize default acceleration, used as fallback */
+	f->funcs.fallback = create_custom_accel_function(default_step,
+							 default_points,
+							 ARRAY_LENGTH(default_points));
+	/* Don't initialize other acceleration functions. Those will be
+	   initialized if the user sets their points, otherwise the fallback
+	   acceleration function is used */
+
+	f->base.interface = &custom_accelerator_interface;
+
+	return &f->base;
+}

AOSC-Tracking/libinput

commit 04975b46184a6afbe2f5dd3646326804d5d5bd61

Constify libinput_config_accel_set_points()
We copy the contents of the double array so let's constify this.

Fixes: 5324f425a163 ("Introduce custom acceleration profile")

Closes #1114

Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1195>

src/libinput.c
@@ -4444,7 +4444,9 @@ libinput_device_config_accel_apply(struct libinput_device *device,
 LIBINPUT_EXPORT enum libinput_config_status
 libinput_config_accel_set_points(struct libinput_config_accel *config,
 				 enum libinput_config_accel_type accel_type,
-				 double step, size_t npoints, double *points)
+				 double step,
+				 size_t npoints,
+				 const double *points)
 {
 	if (config->profile != LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM)
 		return LIBINPUT_CONFIG_STATUS_INVALID;

src/libinput.h
@@ -5775,7 +5775,9 @@ enum libinput_config_accel_type {
 enum libinput_config_status
 libinput_config_accel_set_points(struct libinput_config_accel *accel_config,
 				 enum libinput_config_accel_type accel_type,
-				 double step, size_t npoints, double *points);
+				 double step,
+				 size_t npoints,
+				 const double *points);
 
 /**
  * @ingroup config